1f743d25e0d1f19a5dce107ce415cec03b1c7d64
Mate Soos <soos.mate@gmail.com>
2026-01-22 23:24:41 +0100

    Add my temp generation

---
+++ src/mytemp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (C) 2025 Mate Soos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import os
+import stat
+
+def unique_file(fname_begin, fname_end=".cnf", max_num_files=2700):
+    counter = 1
+    while True:
+        fname = "out/" + fname_begin + '_' + str(counter) + fname_end
+        try:
+            fd = os.open(
+                fname, os.O_CREAT | os.O_EXCL, stat.S_IREAD | stat.S_IWRITE)
+            os.fdopen(fd).close()
+            return str(fname)
+        except OSError:
+            pass
+
+        counter += 1
+        if counter > max_num_files:
+            print("Cannot create unique_file, last try was: %s" % fname)
+            exit(-1)
+
