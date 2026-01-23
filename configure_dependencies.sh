7f71f00458f8722e24a1fe3009b2bf58b727f668
Mate Soos <soos.mate@gmail.com>
2026-01-23 21:17:52 +0100

    Fixing it up

--- configure_dependencies.sh
+++ configure_dependencies.sh
@@ -50,11 +50,11 @@ fi
 if [ "$ONLY_CLEAN" = "yes" ]; then
     echo "c cleaning only unique"
 else
-	mkdir build
-	cd build || exit
-	export PATH=$HOME/.local/bin:$PATH
-	cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to unique succeeded" || exit
-	make clean
+    mkdir build
+    cd build || exit
+    export PATH=$HOME/.local/bin:$PATH
+    cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to unique succeeded" || exit
+    make clean
     make -j14 && echo "c make to unique succeeded" || exit
     if test -f "interpolatingsolver/src/itp."*; then
         echo "c found itp module"
@@ -73,7 +73,7 @@ fi
 
 
 if [ "$all" = "yes" ]; then
-	echo "Going to install all dependencies from the source code"
+    echo "Going to install all dependencies from the source code"
     echo "[Dependencies-Path]" >> "$CFG_FILE"
     source "$CFG_FILE"
 
@@ -82,14 +82,14 @@ if [ "$all" = "yes" ]; then
     make clean
     rm -f libabc.so
     rm -f file_generation_cex
-	rm -f ../../libabc.so
+    rm -f ../../libabc.so
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only abc"
     else
         CFLAGS='-fPIC' make -j14 libabc.so && echo "c make to ABC succeeded" || exit
         gcc -Wall -g -c file_generation_cex.c -o file_generation_cex.o  && echo "c file_generation_cex complied" || exit
         g++ -g -o file_generation_cex file_generation_cex.o ./libabc.so -lm -ldl -lreadline -lpthread && echo "c file_generation_cex linked" || exit
-		ln -s "dependencies/abc/libabc.so" ../../libabc.so
+        ln -s "dependencies/abc/libabc.so" ../../libabc.so
         file_generation_cex=file_generation_cex
         ln -s libabc.so ../../
         if test -f "$file_generation_cex"; then
@@ -113,13 +113,13 @@ if [ "$all" = "yes" ]; then
     fi
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only cmsgen"
-		rm -rf .cache
-		rm -rf build
+        rm -rf .cache
+        rm -rf build
     else
-		mkdir build
-		cd build || exit
-		cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to cmsgen succeeded" || exit
-		make clean
+        mkdir build
+        cd build || exit
+        cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to cmsgen succeeded" || exit
+        make clean
         make -j14 && echo "c make to cmsgen succeeded" || exit
         cmsgen_exe=cmsgen
         if test -f "$cmsgen_exe"; then
@@ -132,45 +132,44 @@ if [ "$all" = "yes" ]; then
             exit
         fi
         echo "c cmsgen and its dependencies are sucessfully installed"
-		cd ..
+        cd ..
     fi
 
     echo "c installing Open-WBO"
     cd "$openwbo" || exit
     make clean
-	if [ "$ONLY_CLEAN" = "yes" ]; then
-		echo "c cleaning only open-wbo"
-		rm -rf .cache
-		rm -f open-wbo_static
-	else
-		make -j14 r && echo "c make to open wbo succeeded" || exit
-		wbo=open-wbo
-		if test -f "$wbo"; then
-			echo "c $wbo exists."
-			openwbo_path=$(realpath $wbo)
-			echo "openwbo_path = " $openwbo_path >> "$CFG_FILE"
-		else
-			if  test -f "$wbo"*; then
-				echo "c $wbo exists."
-				echo "c coping it to dependencies folder"
-				openwbo_path=$(realpath $wbo*)
-				echo "openwbo_path = " $openwbo_path >> "$CFG_FILE"
-			else
-				echo "Error! could not found $wbo"
-				echo "Error! check open-wbo install and follow INSTALL in build_dependencies/open-wbo"
-				exit
-			fi
-		fi
-		echo "c open-wbo is sucessfully installed"
-		cd ..
-	fi
-
+    if [ "$ONLY_CLEAN" = "yes" ]; then
+        echo "c cleaning only open-wbo"
+        rm -rf .cache
+        rm -f open-wbo_static
+    else
+        make -j14 r && echo "c make to open wbo succeeded" || exit
+        wbo=open-wbo
+        if test -f "$wbo"; then
+            echo "c $wbo exists."
+            openwbo_path=$(realpath $wbo)
+            echo "openwbo_path = " $openwbo_path >> "$CFG_FILE"
+        else
+            if  test -f "$wbo"*; then
+                echo "c $wbo exists."
+                echo "c coping it to dependencies folder"
+                openwbo_path=$(realpath $wbo*)
+                echo "openwbo_path = " $openwbo_path >> "$CFG_FILE"
+            else
+                echo "Error! could not found $wbo"
+                echo "Error! check open-wbo install and follow INSTALL in build_dependencies/open-wbo"
+                exit
+            fi
+        fi
+        echo "c open-wbo is sucessfully installed"
+        cd ..
+    fi
 
     echo "c installing picosat"
     cd "$picosat" || exit
     ./configure.sh && echo "c configuration to picosat succeeded" || exit
     if [ "$ONLY_CLEAN" = "yes" ]; then
-		make clean
+        make clean
         echo "c cleaning only picosat"
     else
         make -j14  && echo "c make to picosat succeeded" || exit
@@ -190,7 +189,7 @@ if [ "$all" = "yes" ]; then
     echo "c install preprocess"
     cd "$preprocess" || exit
     cd louvain-community || exit
-	git stash
+    git stash
     if test -d "build"; then
         echo "c manthan-preproces/louvain-community/build dir exists."
         echo "c clearing it"
@@ -198,23 +197,23 @@ if [ "$all" = "yes" ]; then
     fi
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only louvain-community"
-		rm -rf .cache
-		rm -rf build
+        rm -rf .cache
+        rm -rf build
     else
-		patch -p1 < ../../louvain_fix.diff
-		mkdir build
-		cd build || exit
-		cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to louvain-community succeeded" || exit
-		make clean
+        patch -p1 < ../../louvain_fix.diff
+        mkdir build
+        cd build || exit
+        cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to louvain-community succeeded" || exit
+        make clean
         make -j14 && echo "c make to louvain-community succeeded" || exit
         echo "c louvain-community path set done"
-		cd ..
+        cd ..
     fi
     cd ..
 
     echo "c install cryptominisat"
     cd cryptominisat || exit
-	git stash
+    git stash
     if test -d "build"; then
         echo "c manthan-preproces/cryptominisat/build dir exists."
         echo "c clearing it"
@@ -222,16 +221,16 @@ if [ "$all" = "yes" ]; then
     fi
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only cryptominisat"
-		rm -rf .cache
-		rm -rf build
+        rm -rf .cache
+        rm -rf build
     else
-		patch -p1 < ../../cms_fix.diff
-		mkdir build
-		cd build || exit
-		cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DENABLE_PYTHON_INTERFACE=OFF .. && echo "c cmake to cryptominisat succeeded" || exit
-		make clean
+        patch -p1 < ../../cms_fix.diff
+        mkdir build
+        cd build || exit
+        cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DENABLE_PYTHON_INTERFACE=OFF .. && echo "c cmake to cryptominisat succeeded" || exit
+        make clean
         make -j14 && echo "c make to cryptominisat succeeded" || exit
-		cd ..
+        cd ..
     fi
     cd ..
 
@@ -241,18 +240,18 @@ if [ "$all" = "yes" ]; then
         echo "c clearing it"
         rm -r build
     fi
-	git stash
+    git stash
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only preprocess"
-		rm -rf .cache
-		rm -rf build
-		cd ..
+        rm -rf .cache
+        rm -rf build
+        cd ..
     else
-		patch -p1 < ../preprocess_fix.patch
-		mkdir build
-		cd build || exit
-		cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to preprocess succeeded" || exit
-		make clean
+        patch -p1 < ../preprocess_fix.patch
+        mkdir build
+        cd build || exit
+        cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. && echo "c cmake to preprocess succeeded" || exit
+        make clean
         make -j14 && echo "c make to preprocess succeeded" || exit
         preprocess_exe=preprocess
         if test -f "$preprocess_exe"; then
@@ -264,10 +263,9 @@ if [ "$all" = "yes" ]; then
             echo "Error! check preprocess install and follow readme in build_dependencies/preprocess"
             exit
         fi
-		cd ..
+        cd ..
     fi
 else
     echo "Going to use precomplied static binaries from the dependencies/static_bin folder"
 fi
 exit
-
