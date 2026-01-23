b5a5977f38d1a5d9c87976007a32cd769aaeaa01
Mate Soos <soos.mate@gmail.com>
2026-01-23 20:21:16 +0100

    Fixing

--- configure_dependencies.sh
+++ configure_dependencies.sh
@@ -82,12 +82,14 @@ if [ "$all" = "yes" ]; then
     make clean
     rm -f libabc.so
     rm -f file_generation_cex
+	rm -f ../../libabc.so
     if [ "$ONLY_CLEAN" = "yes" ]; then
         echo "c cleaning only abc"
     else
         CFLAGS='-fPIC' make -j14 libabc.so && echo "c make to ABC succeeded" || exit
         gcc -Wall -g -c file_generation_cex.c -o file_generation_cex.o  && echo "c file_generation_cex complied" || exit
         g++ -g -o file_generation_cex file_generation_cex.o libabc.so -lm -ldl -lreadline -lpthread && echo "c file_generation_cex linked" || exit
+		ln -s "dependencies/abc/libabc.so" ../../libabc.so
         file_generation_cex=file_generation_cex
         ln -s libabc.so ../../
         if test -f "$file_generation_cex"; then
